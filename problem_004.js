0a9121c57d6dd587a4c5bd50ea0078c371b5f549
Md Monirul Islam <mmimonir.ctg@gmail.com>
2023-03-04 18:21:30 +0600

    problem 4 solved

--- problem_004.js
+++ problem_004.js
@@ -1,7 +1,12 @@
-function sequence(data){
-  for(i=1; i<data.length; i++){
-if(data[i]> data[i]-1){
-  return false;
-}
+function sequence(data) {
+  for (i = 1; i < data.length; i++) {
+    if (data[i] < data[i - 1]) {
+      return false;
+    }
   }
+  return true;
 }
+
+console.log(sequence([1, 3, 5, 9, 11]));
+console.log(sequence([1, 3, 9, 5, 11, 8]));
+console.log(sequence([1, 3, 5, 9, 11, 12]));
